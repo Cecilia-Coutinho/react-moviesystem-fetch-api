99a9bbe7c258baff07c8d256fac0155c86eb1950
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-05-05 09:59:23 +0200

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Navbar from './Navbar'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Reactklient</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/createtask">New Person</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
