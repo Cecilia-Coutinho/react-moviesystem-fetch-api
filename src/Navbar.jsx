00f7ef732e58b3d645a3b43e82cdab29e31912b5
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-05-09 11:18:03 +0200

    setup logo to be a link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const LogoLink = styled(Link)`
+  color: var(--color-secondary-5);
+  font-size: 32px;
+  font-family: var(--font-family-2);
+  letter-spacing: 2px;
+
+  &:hover {
+    color: var(--color-secondary-5);
+  }
+}
+`;
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Reactklient</h1>
+        <LogoLink to="/">Reactklient</LogoLink>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/createperson">New Person</Link>
