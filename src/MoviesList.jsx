410accb25dc8f04442f6bfd635fd362c98be7a95
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-05-11 13:53:46 +0200

    polishing

--- src/MoviesList.jsx
+++ src/MoviesList.jsx
@@ -127,28 +127,4 @@ const PaginationDetails = styled.div`
   font-weight: 600;
 `;
 
-const SlidesDetails = styled.div`
-display: 'flex';
-    flex-direction: column;
-    width: 100%;
-    padding: 0 10px;
-`;
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 5,
-    slidesToSlide: 5, // optional, default to 1
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2, // optional, default to 1.
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1,
-  },
-};
 
